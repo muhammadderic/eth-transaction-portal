e8716aa1b033b9f92ca97bbf2c1b299a231c7d2b
muhammadderic <muhammad.donny.e@gmail.com>
2025-05-09 11:11:11 +0700

    f: create services UI and dummy transaction UI

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,42 @@
+const TransactionsCard = () => {
+  return (
+    <div className="bg-[#181918] m-4 flex flex-1
+      2xl:min-w-[450px]
+      2xl:max-w-[500px]
+      sm:min-w-[270px]
+      sm:max-w-[300px]
+      min-w-full
+      flex-col p-3 rounded-md hover:shadow-2xl"
+    >
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="display-flex justify-start w-full mb-6 p-2">
+          <p className="text-white text-base">Amount: ETH</p>
+          <>
+            <br />
+            <p className="text-white text-base">Message: </p>
+          </>
+        </div>
+
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className="text-[#37c7da] font-bold">timestamp</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export const Transactions = () => {
   return (
-    <h1>Transactions</h1>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        <h3 className="text-white text-3xl text-center my-2">
+          Connect your account to see the latest transactions
+        </h3>
+
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          <TransactionsCard/>
+        </div>
+      </div>
+    </div>
   )
 }
